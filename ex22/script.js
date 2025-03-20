b2eca99d387575a91e5a0331c05622d55085695a
Marco_Tomasini <marco_tomasini@estudante.sesesenai.org.br>
2025-03-20 12:36:52 -0300

    ADD - exercises 11 to 25, just test files

---
+++ ex22/script.js
@@ -0,0 +1,8 @@
+let text2 = prompt("Digite uma palavra");
+
+
+function contarCaracteres(str) {
+    return str.length;
+  }
+  
+  console.log(text2 + " - " + contarCaracteres(text2) + " caracteres");
